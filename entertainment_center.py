d9cf1caf20feea4a0942f9bcc6d4816617fc3d14
nauqo <nauqo.quan@gmail.com>
2018-03-31 23:29:10 +0200

    Now with local trailer cache!
    Added 2 functions, get_trailer_local and save_trailer.
    
    Updated get_json_from_url with a try/catch exception incase no trailer are found at themoviedb
    
    #get_trailer_local check if the trailer to a movie is on a local file. Return the trailer url if on local file, return False otherwise
    
    #save_trailer just save the trailer and movie id to a localfile.
    
    I should really commit/push more often...

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,4 +1,4 @@
-import urllib, json, media, fresh_tomatoes
+import urllib, json, media, fresh_tomatoes, os,sys
 
 # I am using these constant variables to build correct urls
 
@@ -32,28 +32,39 @@ def create_popular_movie_web_site():
     # An array to be filled with movies
     movies = []
 
+    # A discover call to themoviedb, fetching the top movies
     data = get_json_from_url(DISCOVER_URL)
-    i = 0
     for result in data["results"]:
-        if(i < 9):
-            title = result["title"]
-            poster_url = BASE_POSTER_URL+IMG_W300+result["poster_path"]
+        title = result["title"]
+        poster_url = BASE_POSTER_URL+IMG_W300+result["poster_path"]
+
+        # Sometimes some movies have no trailers, they will not be shown.
+        try:
             trailer = get_movie_trailer_url(result["id"])
             movie = media.Movie(title,poster_url,trailer)
             movies.append(movie)
-            i += 1
+        except IndexError:
+            print("No trailer found, will not put it on the site")
     fresh_tomatoes.open_movies_page(movies)
 
-# themoviedb api doesn't give movie trailers(using discover),
-# so a call using the movie id is needed to get youtube key
-def get_movie_trailer_url(movie_id):
-    movie_url = BASE_URL+MOVIE+str(movie_id)+API+API_KEY+APPEND_VIDEOS
-    data = get_json_from_url(movie_url)
 
-    # Get the first trailer key
-    youtube_key = data["videos"]["results"][0]["key"]
-    trailer_url = YOUTUBE_URL+youtube_key
-    return trailer_url
+def get_movie_trailer_url(movie_id):
+
+    on_local = get_trailer_local(movie_id)
+    if(on_local != False):
+        return on_local
+    else :
+        # themoviedb api doesn't give movie trailers(using discover),
+        # so a call using the movie id is needed to get youtube key
+        movie_url = BASE_URL+MOVIE+str(movie_id)+API+API_KEY+APPEND_VIDEOS
+        data = get_json_from_url(movie_url)
+
+        # Get the first trailer key
+        youtube_key = data["videos"]["results"][0]["key"]
+        trailer_url = YOUTUBE_URL+youtube_key
+
+        save_trailer(movie_id, trailer_url)
+        return trailer_url
 
 def get_json_from_url(url):
     # Fetch json obj from url
@@ -63,5 +74,22 @@ def get_json_from_url(url):
     data = json.loads(response.read())
     response.close()
     return data
+
+
+def get_trailer_local(id):
+    with open(os.path.join(sys.path[0], "youtube_trailer_url.txt"),"r") as file:
+        # Splitting up the string from line to a list, so that the
+        # ID can be checked and the url can be extracted
+        for line in file:
+            data = line.split(" ")
+            if(id == int(data[0])):
+                # Removes \n at the end of the string
+                result = data[1].replace("\n","")
+                return result
+    return False
+
+def save_trailer(id,youtube_url):
+    with open(os.path.join(sys.path[0], "youtube_trailer_url.txt"),"a") as file:
+        file.write(str(id)+" "+youtube_url+"\n")
 
 create_popular_movie_web_site()
