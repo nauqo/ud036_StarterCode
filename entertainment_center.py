1614f1d0322468f2c9d14efbecf044ecd6eee4ad
nauqo <nauqo.quan@gmail.com>
2018-04-02 14:22:46 +0200

    Major fix and improvements
    Now it works without an api key as long as there are saved information in the file movies.txt
    
    #entertainment_center.py : added some more safe controll check with try/catch. removed get_trailer_local() and save_trailer() and moved those into media.py instead.
    
    #media.py : added save_movie_to_file(id) which will save the movie object and information  to a file movies.txt.
    added a staticmethod get_data_from_movie(id,data) which will returns a movie data or all of it.
    
    #movies.txt is just a file which holds information about the movie. Have this seutp:
    id;:title;:poster_url;:trailer_url
    where ;: is the split.
    
    #youtube_trailer_url.txt is removed

--- entertainment_center.py
+++ entertainment_center.py
@@ -31,40 +31,54 @@ IMG_W300 = "/w300";
 def create_popular_movie_web_site():
     # An array to be filled with movies
     movies = []
+    try:
+        # A discover call to themoviedb, fetching the top movies
+        response = get_json_from_url(DISCOVER_URL)
+        for data in response["results"]:
+            title = data["title"]
+            poster_url = BASE_POSTER_URL+IMG_W300+data["poster_path"]
 
-    # A discover call to themoviedb, fetching the top movies
-    data = get_json_from_url(DISCOVER_URL)
-    for result in data["results"]:
-        title = result["title"]
-        poster_url = BASE_POSTER_URL+IMG_W300+result["poster_path"]
+            # Try to fetch from local file first, api request takes longer time
+            try:
+                id = data["id"]
+                on_local = media.Movie.get_data_from_movie(id,"trailer")
+                if(on_local != False):
+                    trailer = on_local
+                    movie = media.Movie(title,poster_url,trailer)
+                else:
+                    trailer = get_movie_trailer_url(id)
+                    movie = media.Movie(title,poster_url,trailer)
+                    movie.save_movie_to_file(id)
+                movies.append(movie)
 
-        # Sometimes some movies have no trailers, they will not be shown.
-        try:
-            trailer = get_movie_trailer_url(result["id"])
+            # Sometimes some movies have no trailers, they will not be shown.
+            except IndexError:
+                print("No trailer found, will not put it on the site")
+
+
+    except IOError:
+        print("Please check your api key, movies from file will be used")
+        file = media.Movie.get_data_from_movie(0,"ALL")
+        for line in file:
+            data = line.split(";:")
+            title = data[1]
+            poster_url = data[2]
+            trailer = data[3]
             movie = media.Movie(title,poster_url,trailer)
             movies.append(movie)
-        except IndexError:
-            print("No trailer found, will not put it on the site")
     fresh_tomatoes.open_movies_page(movies)
-
 
 def get_movie_trailer_url(movie_id):
 
-    on_local = get_trailer_local(movie_id)
-    if(on_local != False):
-        return on_local
-    else :
-        # themoviedb api doesn't give movie trailers(using discover),
-        # so a call using the movie id is needed to get youtube key
-        movie_url = BASE_URL+MOVIE+str(movie_id)+API+API_KEY+APPEND_VIDEOS
-        data = get_json_from_url(movie_url)
+    # themoviedb api doesn't give movie trailers(using discover),
+    # so a call using the movie id is needed to get youtube key
+    movie_url = BASE_URL+MOVIE+str(movie_id)+API+API_KEY+APPEND_VIDEOS
+    data = get_json_from_url(movie_url)
 
-        # Get the first trailer key
-        youtube_key = data["videos"]["results"][0]["key"]
-        trailer_url = YOUTUBE_URL+youtube_key
-
-        save_trailer(movie_id, trailer_url)
-        return trailer_url
+    # Get the first trailer key
+    youtube_key = data["videos"]["results"][0]["key"]
+    trailer_url = YOUTUBE_URL+youtube_key
+    return trailer_url
 
 def get_json_from_url(url):
     # Fetch json obj from url
@@ -74,22 +88,5 @@ def get_json_from_url(url):
     data = json.loads(response.read())
     response.close()
     return data
-
-
-def get_trailer_local(id):
-    with open(os.path.join(sys.path[0], "youtube_trailer_url.txt"),"r") as file:
-        # Splitting up the string from line to a list, so that the
-        # ID can be checked and the url can be extracted
-        for line in file:
-            data = line.split(" ")
-            if(id == int(data[0])):
-                # Removes \n at the end of the string
-                result = data[1].replace("\n","")
-                return result
-    return False
-
-def save_trailer(id,youtube_url):
-    with open(os.path.join(sys.path[0], "youtube_trailer_url.txt"),"a") as file:
-        file.write(str(id)+" "+youtube_url+"\n")
 
 create_popular_movie_web_site()
--- media.py
+++ media.py
@@ -1,6 +1,34 @@
+import os, sys
 class Movie:
 
     def __init__(self,title,poster_image_url,trailer_youtube_url):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+
+    def save_movie_to_file(self,id):
+        # id title poster_url youtube_url\n
+        # Are going to be saved into a file
+        with open(os.path.join(sys.path[0], "movies.txt"),"a") as file:
+            file.write(
+                str(id)+";:"+self.title+";:"+self.poster_image_url+
+                ";:"+self.trailer_youtube_url+"\n")
+
+    @staticmethod
+    def get_data_from_movie(id,data):
+        with open(os.path.join(sys.path[0], "movies.txt"),"r") as file:
+            if(data == "ALL"):
+                return file.readlines()
+            for line in file:
+                res = line.split(";:")
+                if(str(id) == res[0]):
+                    if(data == "title"):
+                        return res[1]
+                    elif(data == "poster"):
+                        return res[2]
+                    # Need to remove the \n at the end of line
+                    elif(data == "trailer"):
+                        return data[3].replace("\n","")
+                    else:
+                        print("Data string not valid, check it, return False")
+        return False
